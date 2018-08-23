8531734bb05ddec8a668cc9260a417c52a0c93aa
Nehal Soni <nehalsoni@Nehals-iMac.local>
2018-08-23 23:47:44 +0100

    lesson 4

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Nehal Soni on 23/08/2018.
+//  Copyright © 2018 Nehal Soni. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Nehal Soni on 23/08/2018.
+//  Copyright © 2018 Nehal Soni. All rights reserved.
+//
+
+import Foundation
+
+// Seques
+let TO_LOGIN = "toLogin"
