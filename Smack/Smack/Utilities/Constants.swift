5967f9fb57d629ea801452134e4c5dbc1e798f9a
Nehal Soni <nehalsoni@Nehals-iMac.local>
2018-08-31 00:00:45 +0100

    lesson 9

--- Smack/Smack/Utilities/Constants.swift
+++ Smack/Smack/Utilities/Constants.swift
@@ -8,7 +8,17 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL constnts
+let BASE_URL = "https://chatsterchat.herokuapp.com/"
+let URL_REGISTER = "\(BASE_URL)account/register"
 // Seques
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//User defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "logged_in"
+let USER_EMAIL = "userEmail"
